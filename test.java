390e5452e6440c7fcc15683cbf9eabb6218d1c95
tester369 <willis_369@yahoo.com>
2017-04-02 12:16:59 -0700

    adding new file

---
+++ test.java
@@ -0,0 +1,17 @@
+import java.io.*;
+import java.util.*;
+
+
+class testing {
+  int one;
+  public testing() {
+    this.one = 1;
+  }
+}
+
+public static void main(String args[]) {
+  testing box = new testing();
+  System.out.println(box.one);
+}
+
+
